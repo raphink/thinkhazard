96fc666b35acd854d593d7f2afc1d250a67924cb
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-08-05 17:31:56 +0200

    SSH variables

--- .deploy.sh
+++ .deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -xe
 
+SSH_OPTS="-o StrictHostKeyChecking=no -i id_rsa"
+SSH_HOST="reprepro@pkg.camptocamp.net"
 SECTION="ci"
 DISTRO="jessie/dev"
 # TODO: Detect/generate that
@@ -9,10 +11,10 @@ PACKAGE="${PACKAGE_NAME}_0.0_amd64.deb"
 if [ -z $TRAVIS_TAG ]; then
   # TODO: pass $TRAVIS_TAG to make deb?
   make deb
-  scp -o StrictHostKeyChecking=no -i id_rsa $PACKAGE reprepro@pkg.camptocamp.net:/var/packages/apt/incoming
-  ssh -o StrictHostKeyChecking=no -i id_rsa reprepro@pkg.camptocamp.net "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} -P optional includedeb ${DISTRO} /var/packages/apt/incoming/${PACKAGE}"
+  scp $SSH_OPTS $PACKAGE $SSH_HOST:/var/packages/apt/incoming
+  ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} -P optional includedeb ${DISTRO} /var/packages/apt/incoming/${PACKAGE}"
 else
   # TODO: Get that from tag
   NEW_DISTRO="jessie/staging"
-  ssh -o StrictHostKeyChecking=no -i id_rsa reprepro@pkg.camptocamp.net "/usr/bin/reprepro -b /var/packages/apt copy ${NEW_DISTRO} ${DISTRO} ${PACKAGE_NAME}"
+  ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt copy ${NEW_DISTRO} ${DISTRO} ${PACKAGE_NAME}"
 fi
