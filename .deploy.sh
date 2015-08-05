79483cc21566cd372d68cf53c8550516ceb160cc
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-08-05 17:19:32 +0200

    SSH_OPTS no work?

--- .deploy.sh
+++ .deploy.sh
@@ -1,6 +1,5 @@
-#!/bin/sh -e
+#!/bin/sh -xe
 
-SSH_OPTS="-o StrictHostKeyChecking=no -i id_rsa reprepro@pkg.camptocamp.net"
 SECTION="ci"
 DISTRO="jessie/dev"
 # TODO: Detect/generate that
@@ -11,9 +10,9 @@ if [ -z $TRAVIS_TAG ]; then
   # TODO: pass $TRAVIS_TAG to make deb?
   make deb
   scp $PACKAGE $SSH_OPTS:/var/packages/apt/incoming
-  ssh $SSH_OPTS "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} -P optional includedeb ${DISTRO} /var/packages/apt/incoming/${PACKAGE}"
+  ssh -o StrictHostKeyChecking=no -i id_rsa reprepro@pkg.camptocamp.net "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} -P optional includedeb ${DISTRO} /var/packages/apt/incoming/${PACKAGE}"
 else
   # TODO: Get that from tag
   NEW_DISTRO="jessie/staging"
-  ssh $SSH_OPTS "/usr/bin/reprepro -b /var/packages/apt copy ${NEW_DISTRO} ${DISTRO} ${PACKAGE_NAME}"
+  ssh -o StrictHostKeyChecking=no -i id_rsa reprepro@pkg.camptocamp.net "/usr/bin/reprepro -b /var/packages/apt copy ${NEW_DISTRO} ${DISTRO} ${PACKAGE_NAME}"
 fi
