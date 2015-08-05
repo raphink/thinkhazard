986984f2d84932acac87e248ada68759b793a4c3
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-08-05 17:49:11 +0200

    No -S for copy

--- .deploy.sh
+++ .deploy.sh
@@ -14,5 +14,5 @@ if [ -z $TRAVIS_TAG ]; then
   scp $SSH_OPTS $PACKAGE $SSH_HOST:/var/packages/apt/incoming
   ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} -P optional includedeb ${DISTRO}/dev /var/packages/apt/incoming/${PACKAGE}"
 else
-  ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} copy ${DISTRO}/${TRAVIS_TAG##*/} ${DISTRO}/dev ${PACKAGE_NAME}"
+  ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt copy ${DISTRO}/${TRAVIS_TAG##*/} ${DISTRO}/dev ${PACKAGE_NAME}"
 fi
