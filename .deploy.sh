8c9cd4216c1939f053a20d4b171999c358f190de
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-08-05 17:41:11 +0200

    Dynamic tags

--- .deploy.sh
+++ .deploy.sh
@@ -3,7 +3,7 @@
 SSH_OPTS="-o StrictHostKeyChecking=no -i id_rsa"
 SSH_HOST="reprepro@pkg.camptocamp.net"
 SECTION="ci"
-DISTRO="jessie/dev"
+DISTRO="jessie"
 # TODO: Detect/generate that
 PACKAGE_NAME="thinkhazard"
 PACKAGE="${PACKAGE_NAME}_0.0_amd64.deb"
@@ -12,9 +12,7 @@ if [ -z $TRAVIS_TAG ]; then
   # TODO: pass $TRAVIS_TAG to make deb?
   make deb
   scp $SSH_OPTS $PACKAGE $SSH_HOST:/var/packages/apt/incoming
-  ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} -P optional includedeb ${DISTRO} /var/packages/apt/incoming/${PACKAGE}"
+  ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} -P optional includedeb ${DISTRO}/dev /var/packages/apt/incoming/${PACKAGE}"
 else
-  # TODO: Get that from tag
-  NEW_DISTRO="jessie/staging"
-  ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt copy ${NEW_DISTRO} ${DISTRO} ${PACKAGE_NAME}"
+  ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} copy ${DISTRO}/${TAG##*/} ${DISTRO}/dev ${PACKAGE_NAME}"
 fi
